407aea72419632793001f76f9c316685525b3219
QinShiLin <623200732@qq.com>
2018-01-05 16:51:25 +0800

    回滚

--- src/com/zhiyou100/gt/Test1.java
+++ src/com/zhiyou100/gt/Test1.java
@@ -1,8 +1,8 @@
 package com.zhiyou100.gt;
 
-public class Test02 {
+public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("新增文件");
+		System.out.println("新建测试项目");
 	}
 }
